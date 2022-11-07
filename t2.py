1a0f038db4188a19a8d5b372ff97a16dd40c93bf
Roman Berger <romanberger001@gmail.com>
2022-11-07 21:55:20 +0000

    tried finishing 2.ii, not working correctly

--- t2.py
+++ t2.py
@@ -1,14 +1,20 @@
 from scipy import signal
 import matplotlib.pyplot as plt
 
+# TO DO
+# - create the DT systems automatically from the coeff_list
+# - labels & legends in plot
+# - plot certain frequency range
+
 if __name__ == '__main__':
     gamma = 100;
     alpha = 0.2
     beta = 100
+    freqs = [i/10 for i in range(1,1000000)]
 
     # continuous time transfer function
     sys = signal.TransferFunction([gamma], [1, alpha, beta])
-    w, mag, phase = signal.bode(sys)
+    w, mag, phase = signal.bode(sys, w=freqs)
 
     #DT transfer function
     T_list = [0.01,0.001,0.0001]
@@ -22,18 +28,37 @@ if __name__ == '__main__':
     DT_sys_1 = signal.dlti([coeff_list[0][0][0],coeff_list[0][0][1],coeff_list[0][0][2]],
                            [coeff_list[0][1][0],coeff_list[0][1][1], coeff_list[0][1][2]],
                            dt = coeff_list[0][2])
+
     DT_w_1, DT_mag_1, DT_phase_1 = signal.dbode(DT_sys_1)
 
-    plt.figure()
-    plt.semilogx(w, mag)    # Bode magnitude plot
-    plt.semilogx(DT_w_1,DT_mag_1)
-    plt.grid()
+    DT_sys_2 = signal.dlti([coeff_list[1][0][0],coeff_list[1][0][1],coeff_list[1][0][2]],
+                           [coeff_list[1][1][0],coeff_list[1][1][1], coeff_list[1][1][2]],
+                           dt = coeff_list[1][2])
+
+    DT_w_2, DT_mag_2, DT_phase_2 = signal.dbode(DT_sys_2)
+
+    DT_sys_3 = signal.dlti([coeff_list[2][0][0], coeff_list[2][0][1], coeff_list[2][0][2]],
+                           [coeff_list[2][1][0], coeff_list[2][1][1], coeff_list[2][1][2]],
+                           dt = coeff_list[2][2])
+
+    DT_w_3, DT_mag_3, DT_phase_3 = signal.dbode(DT_sys_3)
+
+    f1 = plt.figure()
+    plt.subplot(211)
+    plt.semilogx(w, mag, lw = 1)    # Bode magnitude plot
+    plt.semilogx(DT_w_1,DT_mag_1,lw = 1)
+    plt.semilogx(DT_w_2,DT_mag_2,lw = 1)
+    plt.semilogx(DT_w_3,DT_mag_3,lw = 1)
+    plt.grid(visible = 'true',axis = 'both', which = 'both', linestyle='--', figure = f1)
     plt.xlabel("frequency [rad/s]")
     plt.ylabel("Magnitude [dB]")
-    plt.figure()
-    plt.semilogx(w, phase)  # Bode phase plot
-    plt.semilogx(DT_w_1,DT_phase_1)
-    plt.grid()
+    plt.subplot(212)
+    plt.semilogx(w, phase, lw = 1)  # Bode phase plot
+    plt.semilogx(DT_w_1,DT_phase_1, lw = 1)
+    plt.semilogx(DT_w_2,DT_phase_2, lw = 1)
+    plt.semilogx(DT_w_3,DT_phase_3, lw = 1)
+    plt.ylabel("Phase [deg]")
+    plt.grid(visible = 'true',axis = 'both', which = 'both', linestyle='--', figure = f1)
     plt.show()
 
     sys = sys.to_zpk()
