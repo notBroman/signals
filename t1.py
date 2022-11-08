7eacddaf361a0611dbbf5dead504e16576def5d2
Roman Berger <romanberger001@gmail.com>
2022-11-08 16:11:33 +0000

    progress day 2

--- t1.py
+++ t1.py
@@ -1,6 +1,15 @@
-import scipy
+from scipy import signal
 import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
+    R = 80
+    C = 280 *10^(-5)
+    L1 = 0.01
+    L2 = 0.02
+    sys = signal.lti([(L1+L2)/R,1],[L2*(L1+L2)/R,C*L1*L2+L1+L2,C*L1*R+L2*(L1+L2)/R,2*L1+L2,R])
+    w, mag, phase = signal.bode(sys)
 
+    plt.figure()
+    plt.semilogx(w, mag)
+    plt.show()
--- t2.py
+++ t2.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     freqs = [i/10 for i in range(1,1000000)]
 
     # continuous time transfer function
-    sys = signal.TransferFunction([gamma], [1, alpha, beta])
+    sys = signal.lti([gamma], [1, alpha, beta])
     w, mag, phase = signal.bode(sys, w=freqs)
 
     #DT transfer function
@@ -21,27 +21,26 @@ if __name__ == '__main__':
     # create a list of lists with num, den in them
     coeff_list= list()
     for i in range(3):
-        t2 = T_list[i]**2
         t = T_list[i]
-        coeff_list.append([[1*100*t2,2*100*t2,1*100*t2],[4+0.4*t+100*t2,-8+200*t2,4-0.4*t+100*t2],t])
+        t2 = t*t
+        coeff_list.append([[1*gamma*t2,2*gamma*t2,1*gamma*t2],[4+2*alpha*t+beta*t2,-8+2*beta*t2,4-2*alpha*t+beta*t2],t])
+    print(coeff_list[0])
 
-    DT_sys_1 = signal.dlti([coeff_list[0][0][0],coeff_list[0][0][1],coeff_list[0][0][2]],
-                           [coeff_list[0][1][0],coeff_list[0][1][1], coeff_list[0][1][2]],
-                           dt = coeff_list[0][2])
-
+    DT_sys_1 = signal.dlti(coeff_list[0][0],coeff_list[0][1], dt = coeff_list[0][2])
     DT_w_1, DT_mag_1, DT_phase_1 = signal.dbode(DT_sys_1)
+    dt_sys = signal.cont2discrete(sys,dt= 0.01)
+    # print(dt_sys)
 
-    DT_sys_2 = signal.dlti([coeff_list[1][0][0],coeff_list[1][0][1],coeff_list[1][0][2]],
-                           [coeff_list[1][1][0],coeff_list[1][1][1], coeff_list[1][1][2]],
-                           dt = coeff_list[1][2])
-
+    DT_sys_2 = signal.dlti(coeff_list[1][0],coeff_list[1][1],dt = coeff_list[1][2])
     DT_w_2, DT_mag_2, DT_phase_2 = signal.dbode(DT_sys_2)
 
-    DT_sys_3 = signal.dlti([coeff_list[2][0][0], coeff_list[2][0][1], coeff_list[2][0][2]],
-                           [coeff_list[2][1][0], coeff_list[2][1][1], coeff_list[2][1][2]],
-                           dt = coeff_list[2][2])
-
+    DT_sys_3 = signal.dlti(coeff_list[2][0],coeff_list[2][1], dt = coeff_list[2][2])
     DT_w_3, DT_mag_3, DT_phase_3 = signal.dbode(DT_sys_3)
+
+
+    print(DT_sys_1)
+    print(DT_sys_2)
+    print(DT_sys_3)
 
     f1 = plt.figure()
     plt.subplot(211)
